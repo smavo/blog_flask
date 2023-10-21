18414ef9b58c0e59c75a0b2c52dbe87539d5e170
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-10-20 22:21:49 -0500

    Add template login, register

--- core/auth.py
+++ core/auth.py
@@ -5,12 +5,12 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/register')
 def register():
-    return 'Pagina de Registro de Usuario'
+    return render_template('auth/register.html')
 
 
 @bp.route('/login')
 def login():
-    return 'Pagina de Login del Blog'
+    return render_template('auth/login.html')
 
 
 @bp.route('/profile')
