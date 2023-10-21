a416500fce2b02da9f5187b1c7eeadf3c2e68b42
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-10-20 22:27:54 -0500

    Add archivo de configuracion

---
+++ config.py
@@ -0,0 +1,5 @@
+
+class Config:
+    DEBUG = True
+    SECRET_KEY = 'smavodev'
+
--- core/__init__.py
+++ core/__init__.py
@@ -5,6 +5,8 @@ def create_app():
     # Crear aplicación de flask
     app = Flask(__name__)
 
+    app.config.from_object('config.Config')
+
     # Registrar vistas
     from core import home
     app.register_blueprint(home.bp)
