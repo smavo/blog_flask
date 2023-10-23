682574f9e9861b20b0fff9ac7342bb30dfa19995
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-10-23 16:30:22 -0500

    Mostrar los Posts en el Index, Ver Posts a detalle

--- core/home.py
+++ core/home.py
@@ -1,13 +1,24 @@
 from flask import Blueprint, render_template, request
+from .models import User, Post
 
 bp = Blueprint('home', __name__)
 
 
+def get_user(id):
+    user = User.query.get_or_404(id)
+    return user
+
+
 @bp.route('/')
 def index():
-    return render_template('index.html')
+    # posts = Post.query.all()
+    posts = Post.query.order_by(Post.created.desc()).limit(5).all()  # Los últimos 5 Mostrar
+    return render_template('index.html', posts=posts, get_user=get_user)
 
 
-@bp.route('/blog')
-def blog():
-    return 'Pagina del Blog'
+@bp.route('/blog/<url>')
+def blog(url):
+    post = Post.query.filter_by(url=url).first()
+    return render_template('blog.html', post=post, get_user=get_user)
+
+
