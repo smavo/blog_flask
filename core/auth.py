0262df5c552f073701dcd17522cde66603115ef0
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-10-22 22:45:50 -0500

    Add logout, Redireccion de paginas internas, validacion de session, Add Templates

--- core/auth.py
+++ core/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, url_for, redirect, flash, session
+from flask import Blueprint, render_template, request, url_for, redirect, flash, session, g
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from core import db
@@ -6,8 +6,11 @@ from core import db
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
-@bp.route('/register', methods = ('GET', 'POST'))
+@bp.route('/register', methods=('GET', 'POST'))
 def register():
+    if g.user:
+        return redirect(url_for('post.posts'))
+
     if request.method == 'POST':
         username = request.form.get('username')
         email = request.form.get('email')
@@ -34,6 +37,9 @@ def register():
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
+    if g.user:
+        return redirect(url_for('post.posts'))
+
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
@@ -54,6 +60,35 @@ def login():
         flash(error)
 
     return render_template('auth/login.html')
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.get_or_404(user_id)
+
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home.index'))
+
+
+import functools
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+
+    return wrapped_view
 
 
 @bp.route('/profile')
--- core/posts.py
+++ core/posts.py
@@ -1,11 +1,18 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, session
+from .auth import login_required
+from .models import Post
+from core import db
 
-bp = Blueprint('post', __name__, url_prefix = '/post')
+bp = Blueprint('post', __name__, url_prefix='/post')
 
 
 @bp.route('/posts')
+@login_required
 def posts():
-    return 'Pagina de todos los posts'
+    user_id = session.get('user_id')
+    posts = Post.query.filter_by(author=user_id).all()
+    # posts = Post.query.all()
+    return render_template('admin/posts.html', posts=posts)
 
 
 @bp.route('/create')
@@ -21,4 +28,3 @@ def update():
 @bp.route('/delete')
 def delete():
     return 'Eliminar Post'
-
